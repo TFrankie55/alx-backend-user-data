9c153eb5ff1f8cec00ad57a763fc9b0d84dda894
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-03-07 04:24:21 +0100

    Update index.py

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -14,6 +14,24 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+      a dummy route to raise a
+      401 (Unauthorized) error
+    """
+    abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+      a dummy route to raise a
+      403 (Forbidden) error
+    """
+    abort(403, description="Forbidden")
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
